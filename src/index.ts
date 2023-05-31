a189c28584ce66aaa9f87f17500dc0204d9ecce2
JasubiPL <gameportal7@hotmail.com>
2023-05-31 17:59:56 -0600

    Types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("Changed2")
+// Variables son tipadas y no se puede cambiar el tipo de dato una vez asignado
+
+let nombre: string = 'jasubip'
+
+console.log(`Hola ${nombre}`)
+console.log(`Si eres ${nombre} ?`)
+
+const PI: number = 3.1416
+
+/**
+ * con any se puede cambiar el tipo de dato
+ * Si no se indica el typo de dato, por defecto sera "any"
+*/
+var apellido: any = "piñeyro"
+
+apellido = 3
+
+console.log(`Tu apellido es ${apellido}`)
+
+//variables booleana
+var error: boolean;
+error = false
+
+console.log(`El error es: ${error}`)
+
+//Instanciacion Multiple
+
+let a:string, b:number ,c:boolean
+
+a = "texto"
+b = 22
+c = true
+
+console.log(a, b, c)
+
+//Tipos complejos
+let listaTareas: string[] = ["tarea1", "tarea2"]
