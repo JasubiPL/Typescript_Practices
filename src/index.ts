bc5535357420452e9f570f84a5370d80bf673ed3
JasubiPL <gameportal7@hotmail.com>
2023-06-06 17:30:21 -0600

    Finish function

--- src/index.ts
+++ src/index.ts
@@ -50,3 +50,58 @@ function datosContacto(nombre: string, email: string, edad?: number,){
 
 datosContacto("jasubi", "Jasubi.com")
 datosContacto("jasubi", "Jasubi.com", 26)
+
+/**
+ * 
+ * @param apellidos Apellidos del usuario
+ * @param nombre nombre del usuario
+ * @returns nombre completo del usuario
+ */
+function ejemploReturn(apellidos: string, nombre?: string): string{
+  return `${nombre} ${apellidos}`
+}
+
+const nombreCompleto = ejemploReturn("Piñeyro","Jasubi")
+
+console.log(nombreCompleto)
+
+/**
+ * 
+ * @param nombres es una lista de nombres en string
+ */
+
+function ejemploMultiParam(...nombres: string[]): void{
+  nombres.forEach((nombre) =>{
+    console.log(nombre)
+  })
+}
+
+ejemploMultiParam("Jasubi", "Lehem", "Piñeyro", "Legaspi")
+
+//Arrow function
+
+type Empleado = {
+  nombre: string,
+  apellido: string,
+  edad: number
+}
+
+let empleadoMartin: Empleado = {
+  nombre: "Martin",
+  apellido: "Santa Cruz",
+  edad: 30
+}
+
+const mostrarEmpleado = (empleado: Empleado): string => `${empleado.nombre} tiene ${empleado.edad} años`
+
+console.log(mostrarEmpleado(empleadoMartin))
+
+const datosEmpleado = (empleado: Empleado): string =>{
+  if(empleado.edad > 70){
+    return `Empleado ${empleado.nombre} esta en edad de juvilacion`
+  }else{
+    return `Empleado ${empleado.nombre} esta en edad laboral`
+  }
+}
+
+console.log(datosEmpleado(empleadoMartin))
